6f2e525a41e8c2b25ef4bb2b9a809a8d787d0f62
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-04-24 20:02:06 +0530

    60 Key Creation

--- services/cache.js
+++ services/cache.js
@@ -4,7 +4,14 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
   console.log("I am about to run Query");
-  console.log(this.getQuery()); // Query information (Query Params: find, where, _id, aggregate, group etc)
-  console.log(this.mongooseCollection.name);// Collection (Model) name : Users / Blogs
+//   console.log(this.getQuery()); // Query information (Query Params: find, where, _id, aggregate, group etc)
+//   console.log(this.mongooseCollection.name);// Collection (Model) name : Users / Blogs
+
+// Creating Unique key for Unique Queries
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  });
+  
+  console.log(key);
   return exec.apply(this, arguments);
 };
