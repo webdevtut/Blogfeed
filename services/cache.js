b3e36cbb1f748fa7ea246d98d168cfa74f8af509
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-04-24 20:08:25 +0530

    61 Restoring Redis Config

--- services/cache.js
+++ services/cache.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require('util');
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
@@ -11,7 +16,7 @@ mongoose.Query.prototype.exec = function () {
   const key = Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name
   });
-  
+
   console.log(key);
   return exec.apply(this, arguments);
 };
