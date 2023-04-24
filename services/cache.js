795058dbc981d15d8e4fafde13cdb46d8185da83
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-04-24 19:53:47 +0530

    59 Unique Keys

--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,7 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
   console.log("I am about to run Query");
+  console.log(this.getQuery()); // Query information (Query Params: find, where, _id, aggregate, group etc)
+  console.log(this.mongooseCollection.name);// Collection (Model) name : Users / Blogs
   return exec.apply(this, arguments);
 };
