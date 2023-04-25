54f7fc52e8d1f20e6f31cf4a652dadaa84080d1d
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-04-25 13:43:57 +0530

    69 Nested Hashes

--- services/cache.js
+++ services/cache.js
@@ -3,12 +3,13 @@ const redis = require("redis");
 const util = require('util');
 const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
   return this; // required for creating chain function
 }
 
@@ -25,7 +26,7 @@ mongoose.Query.prototype.exec = async function () {
 
   // See if we have value for 'key' in redis
 
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // If Yes, return that
 
@@ -42,7 +43,7 @@ mongoose.Query.prototype.exec = async function () {
    // .validate shows that returned result is function and not plain JSON Object in order to store in redis we have to convert into JSON
     //  console.log(result.validate); 
 
-    client.set(key, JSON.stringify(result), 'EX', 60); // storing cache value
+    client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 60); // storing cache value
 
     console.log("Uncached ♠");
 
