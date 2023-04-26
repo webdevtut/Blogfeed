0f398e1626ac587330b0b94ed28fcff1e92e0608
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-04-26 16:53:01 +0530

    76 Launching Chromium Instances

--- test/header.test.js
+++ test/header.test.js
@@ -1,5 +1,16 @@
+const puppeteer = require('puppeteer');
+
 test('Adds two numbers', () => {
     const sum = 1 + 2;
 
     expect(sum).toEqual(3);
+});
+
+test('We can launch browser', async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+
+    expect(page).toBeDefined();
 });
