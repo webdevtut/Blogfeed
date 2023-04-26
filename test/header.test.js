be1728c02f8bfa14f4997b4c74af9eab3caeb35a
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-04-26 17:59:21 +0530

    80 DRY Tests

--- test/header.test.js
+++ test/header.test.js
@@ -1,17 +1,16 @@
 const puppeteer = require('puppeteer');
 
-test('Adds two numbers', () => {
-    const sum = 1 + 2;
-
-    expect(sum).toEqual(3);
+let browser, page;
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+            headless: false
+        });
+    page = await browser.newPage();
+        await page.goto('localhost:3000');
 });
 
+
 test('We can launch browser', async () => {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
-    await page.goto('localhost:3000');
 
     const text = await page.$eval('img.logoImg', el => el.innerHTML);
 
