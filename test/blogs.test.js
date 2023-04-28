be92be5bc380cb44b51a9650d2d8d412d2022014
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-04-28 20:44:49 +0530

    115 Asserting Validation Errors

--- test/blogs.test.js
+++ test/blogs.test.js
@@ -17,7 +17,7 @@ describe('When logged in', async () => {
     beforeEach(async () => {
         await page.login();
         await page.click('a.btn-floating');
-    })
+    });
 
 
     test('can see blog create form', async () => {
@@ -25,6 +25,20 @@ describe('When logged in', async () => {
         const label = await page.getContentsOf('form label')
     
         expect(label).toEqual("Blog Title");
+    });
+
+    describe('And using invalid inputs', async () => {
+
+        beforeEach(async () => {
+            await page.click('form button');
+        });
+
+        test('the form shows an error message', async() => {
+            const titleError = await page.getContentsOf('.title .red-text');
+            const contentError = await page.getContentsOf('.content .red-text');
+            expect(titleError).toEqual("You must provide a value");
+            expect(contentError).toEqual("You must provide a value");
+        });
     });
 
 
