70b1576e25be0d607b1ec570e17ceb7f47b9dfcf
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-04-28 19:53:34 +0530

    111 Asserting Form Display

--- test/blogs.test.js
+++ test/blogs.test.js
@@ -14,4 +14,8 @@ afterEach(async () => {
 test('When logged in, can see blog create form', async () => {
 
     await page.login();
+    await page.click('a.btn-floating');
+    const label = await page.getContentsOf('form label')
+
+    expect(label).toEqual("Blog Title");
 });
