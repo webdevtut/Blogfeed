6d086963c621b1fcbb59f27861959690c247d9ed
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-04-28 16:19:00 +0530

    104 Combining the Page and Browser

---
+++ test/helpers/page.js
@@ -0,0 +1,21 @@
+const puppeteer = require("puppeteer");
+
+class CustomPage {
+  static async build() {
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    const customPage = new CustomPage(page);
+    return new Proxy(customPage, {
+      get: function (target, property) {
+        return target[property] || page[property] || browser[property];
+      },
+    });
+  }
+  constructor(page) {
+    this.page = page;
+  }
+}
+
+module.exports = CustomPage;
