3dd120b2698e78fc63aa7ff1f3671c4bf2784aa4
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2023-05-04 12:31:25 +0530

    136 CI Repo setup before push

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  redisURI: process.env.REDIS_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
